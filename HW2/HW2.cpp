9ad7aa70cc8dc5545ab65e8cff77476f4163add4
neilmmalu <neilmmalu@gmail.com>
2020-02-27 13:57:10 -0500

    Orders successfully generated

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -16,7 +16,9 @@ time_t seed = time(0);
 
 
 
-vector<int> buffer = {0, 0, 0, 0};
+vector<int> buffer {0, 0, 0, 0};
+
+vector<int> fidelity{ 0, 0, 0, 0 };
 
 vector<int> generateLoadOrder(){
 	vector<int> load{0, 0, 0, 0};
@@ -53,7 +55,7 @@ vector<int> generatePickupOrder(){
         total += pickup[i];
         comp -= pickup[i];
         i++;
-        if(i == 4){
+        if(i == 3){
             pickup[i] = 5 - total;
             break;
         }
@@ -76,44 +78,47 @@ vector<int> generatePickupOrder(){
 void PartWorker(int i){
     while(1){
         vector<int> loadOrder = generateLoadOrder();
+		
+		//Generate time done
+		
+		int moveTime = loadOrder[0]*20 + loadOrder[1]*30 + loadOrder[2]*40 + loadOrder[3]*50;
+		this_thread::sleep_for(chrono::microseconds(moveTime));
+		//Move time done
+
+		//compete for the lock
         unique_lock<mutex> lock(m1);
 
-        int moveTime = 0;
-        this_thread::sleep_for(chrono::microseconds(moveTime));
 
-        if(cv1.wait_for(lock, chrono::microseconds(600))){
-            if(loadOrder[0] + buffer[0] <= 6){
-                buffer[0] += loadOrder[0];
-                moveTime += loadOrder[0]*20;
-                loadOrder[0] = 0;
-            }
+		if (cv1.wait_until(lock, chrono::system_clock::now() + chrono::microseconds(600), [loadOrder] { 
+			return loadOrder[0] + buffer[0] <= 6 || loadOrder[1] + buffer[1] <= 5 || loadOrder[2] + buffer[2] <= 4 || loadOrder[3] + buffer[3] <= 3;
+			})) {
 
-            if(loadOrder[1] + buffer[1] <= 5){
-                buffer[1] += loadOrder[1];
-                moveTime += loadOrder[1]*30;
-                loadOrder[1] = 0;
-            }
+			if (loadOrder[0] + buffer[0] <= 6) {
+				buffer[0] += loadOrder[0];
+				loadOrder[0] = 0;
+			}
 
-            if(loadOrder[2] + buffer[2] <= 4){
-                buffer[2] += loadOrder[2];
-                moveTime += loadOrder[2]*40;
-                loadOrder[2] = 0;
-            }
+			if (loadOrder[1] + buffer[1] <= 5) {
+				buffer[1] += loadOrder[1];
+				loadOrder[1] = 0;
+			}
 
-            if(loadOrder[3] + buffer[3] <= 3){
-                buffer[3] += loadOrder[3];
-                moveTime += loadOrder[3]*50;
-                loadOrder[3] = 0;
-            }
+			if (loadOrder[2] + buffer[2] <= 4) {
+				buffer[2] += loadOrder[2];
+				loadOrder[2] = 0;
+			}
 
-            this_thread::sleep_for(chrono::microseconds(moveTime));
-
-        }
-        else{
-            this_thread::sleep_for(chrono::microseconds(moveTime));
-        }
-
-        cv2.notify_one();
+			if (loadOrder[3] + buffer[3] <= 3) {
+				buffer[3] += loadOrder[3];
+				loadOrder[3] = 0;
+			}
+		}
+		else {
+			//Discard?
+		}
+		cv2.notify_one();
+		
+        
 
     }
 }
@@ -121,44 +126,11 @@ void PartWorker(int i){
 void ProductWorker(int i){
     while(1){
         vector<int> pickupOrder = generatePickupOrder();
-        unique_lock<mutex> lock(m1);
 
-        int moveTime = 0;
         
-        //wait for the lock until timeout
-        if(cv2.wait_for(lock, chrono::microseconds(1000))){
-            if(buffer[0] > pickupOrder[0]){
-                buffer[0] -= pickupOrder[0];
-                moveTime += pickupOrder[0]*20;
-                pickupOrder[0] = 0;
-            }
+		//unique_lock<mutex> lock(m1);
 
-            if(buffer[1] > pickupOrder[1]){
-                buffer[1] -= pickupOrder[1];
-                moveTime += pickupOrder[1]*30;
-                pickupOrder[1] = 0;
-            }
-
-            if(buffer[2] > pickupOrder[2]){
-                buffer[2] -= pickupOrder[2];
-                moveTime += pickupOrder[2]*40;
-                pickupOrder[2] = 0;
-            }
-
-            if(buffer[3] > pickupOrder[3]){
-                buffer[3] -= pickupOrder[3];
-                moveTime += pickupOrder[3]*50;
-                pickupOrder[3] = 0;
-            }
-
-            this_thread::sleep_for(chrono::microseconds(moveTime));
-
-        }
-        else{
-            this_thread::sleep_for(chrono::microseconds(moveTime));
-        }
-
-        cv2.notify_one();
+        
 
     }
 }
@@ -171,6 +143,7 @@ int main(){
     //n: number of Product Workers
     //m>n
 
+	
     thread partW[m];
     thread prodW[n];
     for (int i = 0; i < n; i++){
@@ -183,7 +156,7 @@ int main(){
     }
 
 
-    /* Join the threads to the main threads */
+    /* Join the threads to the main threads*/ 
     for (int i = 0; i < n; i++) {
         partW[i].join();
         prodW[i].join();
@@ -192,7 +165,8 @@ int main(){
     for (int i = n; i < m; i++) {
         partW[i].join();
     }
-
+	
+	
     cout << "Finish!" << endl;
     return 0;
 }
