799a3ad1ca3509af5d996b63913f01b09288a9be
neilmmalu <neilmmalu@gmail.com>
2020-02-28 14:16:32 -0500

    hope this works

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -28,17 +28,17 @@ bool pushBuffer(vector<int> loadOrder) {
 	return loadOrder[0] + buffer[0] <= 6 || loadOrder[1] + buffer[1] <= 5 || loadOrder[2] + buffer[2] <= 4 || loadOrder[3] + buffer[3] <= 3;
 }
 
-bool pushBufferFull(vector<int> loadOrder) {
-	return loadOrder[0] + buffer[0] <= 6 && loadOrder[1] + buffer[1] <= 5 && loadOrder[2] + buffer[2] <= 4 && loadOrder[3] + buffer[3] <= 3;
-}
+//bool pushBufferFull(vector<int> loadOrder) {
+//	return loadOrder[0] + buffer[0] <= 6 && loadOrder[1] + buffer[1] <= 5 && loadOrder[2] + buffer[2] <= 4 && loadOrder[3] + buffer[3] <= 3;
+//}
 
 bool pullBuffer(vector<int> pickupOrder) {
 	return pickupOrder[0] <= buffer[0] || pickupOrder[1] <= buffer[1] || pickupOrder[2] <= buffer[2] || pickupOrder[3] <= buffer[3];
 }
 
-bool pullBufferFull(vector<int> pickupOrder) {
-	return pickupOrder[0] <= buffer[0] && pickupOrder[1] <= buffer[1] && pickupOrder[2] <= buffer[2] && pickupOrder[3] <= buffer[3];
-}
+//bool pullBufferFull(vector<int> pickupOrder) {
+//	return pickupOrder[0] <= buffer[0] && pickupOrder[1] <= buffer[1] && pickupOrder[2] <= buffer[2] && pickupOrder[3] <= buffer[3];
+//}
 
 vector<int> generateLoadOrder(){
 	vector<int> load{0, 0, 0, 0};
@@ -105,18 +105,21 @@ void PartWorker(int i){
 		//compete for the lock
         unique_lock<mutex> lock(m1);
 
+		int waits = 0;
+
 		chrono::system_clock::time_point start = chrono::system_clock::now();
 		wait:
 		if (cv1.wait_until(lock, start + chrono::microseconds(timeOut), [loadOrder] { return pushBuffer(loadOrder); })) {
 			//Will enter here if predicate is true
 			//If predicate is false, will sleep
 			//If can fit into buffer, will try to fit as much as possible
-            
-			cout << "Current Time: " << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now() - prog_start).count() << "us" << endl;
+			chrono::system_clock::time_point curr_time = chrono::system_clock::now();
+			cout << "Current Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - prog_start).count() << "us" << endl;
 			cout << "Part Worker ID: " << i << endl;
 			cout << "Iteration: " << endl;
-			cout << "Status: New Load Order" << endl;
-			cout << "Accumulated Wait Time: " << chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now() - start).count() << "us" << endl;
+			if (waits == 0) cout << "Status: New Load Order" << endl;
+			else cout << "Status: Wakeup-Notified" << endl;
+			cout << "Accumulated Wait Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - start).count() << "us" << endl;
 			cout << "Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
 			cout << "Load Order: (" << loadOrder[0] << ", " << loadOrder[1] << ", " << loadOrder[2] << ", " << loadOrder[3] << ")" << endl;
 
@@ -142,9 +145,11 @@ void PartWorker(int i){
 
 			cout << "Updated Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
 			cout << "Updated Load Order: (" << loadOrder[0] << ", " << loadOrder[1] << ", " << loadOrder[2] << ", " << loadOrder[3] << ")" << endl;
+			waits++;
 
 			if(loadOrder != fidelity){
 				//put it back to sleep
+				//Not enough space. Alert prod
 				cv2.notify_one();
 				goto wait;
 			}
@@ -155,21 +160,56 @@ void PartWorker(int i){
 			//Timeout condition
 			//Check if buffer can fit entire remaining load
 			//If not then discard the entire thing
-			if (pushBufferFull(loadOrder)) {
-				buffer[0] += loadOrder[0];
+
+			chrono::system_clock::time_point curr_time = chrono::system_clock::now();
+			cout << "Current Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - prog_start).count() << "us" << endl;
+			cout << "Part Worker ID: " << i << endl;
+			cout << "Iteration: " << endl;
+			cout << "Status: Wakeup-Timeout" << endl;
+			cout << "Accumulated Wait Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - start).count() << "us" << endl;
+			//cout << "Accumulated Wait Time: " << PART_TIMEOUT << "us" << endl;
+			cout << "Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Load Order: (" << loadOrder[0] << ", " << loadOrder[1] << ", " << loadOrder[2] << ", " << loadOrder[3] << ")" << endl;
+
+
+			if (pushBuffer(loadOrder)) {
+				/*buffer[0] += loadOrder[0];
 				loadOrder[0] = 0;
 				buffer[1] += loadOrder[1];
 				loadOrder[1] = 0;
 				buffer[2] += loadOrder[2];
 				loadOrder[2] = 0;
 				buffer[3] += loadOrder[3];
-				loadOrder[3] = 0;
+				loadOrder[3] = 0;*/
+
+				if (loadOrder[0] + buffer[0] <= 6) {
+					buffer[0] += loadOrder[0];
+					loadOrder[0] = 0;
+				}
+
+				if (loadOrder[1] + buffer[1] <= 5) {
+					buffer[1] += loadOrder[1];
+					loadOrder[1] = 0;
+				}
+
+				if (loadOrder[2] + buffer[2] <= 4) {
+					buffer[2] += loadOrder[2];
+					loadOrder[2] = 0;
+				}
+
+				if (loadOrder[3] + buffer[3] <= 3) {
+					buffer[3] += loadOrder[3];
+					loadOrder[3] = 0;
+				}
 			}
 			else {
 				//discard
 				moveTime = loadOrder[0] * 20 + loadOrder[1] * 30 + loadOrder[2] * 40 + loadOrder[3] * 50;
 				this_thread::sleep_for(chrono::microseconds(moveTime));
 			}
+
+			cout << "Updated Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Updated Load Order: (" << loadOrder[0] << ", " << loadOrder[1] << ", " << loadOrder[2] << ", " << loadOrder[3] << ")" << endl;
 			
 			//PRINT
 
@@ -187,6 +227,7 @@ void ProductWorker(int i){
         vector<int> pickupOrder = generatePickupOrder();
 		unique_lock<mutex> lock(m1);
 
+		int waits = 0;
 		chrono::system_clock::time_point start = chrono::system_clock::now();
 		wait:
 		if (cv2.wait_until(lock, start + chrono::microseconds(timeOut), [pickupOrder] { return pullBuffer(pickupOrder); }))
@@ -194,6 +235,15 @@ void ProductWorker(int i){
 			//Will enter here if predicate is true
 			//If predicate is false, will sleep
 			//If can pull from buffer, will try to pull as much as possible
+			chrono::system_clock::time_point curr_time = chrono::system_clock::now();
+			cout << "Current Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - prog_start).count() << "us" << endl;
+			cout << "Product Worker ID: " << i << endl;
+			cout << "Iteration: " << endl;
+			if (waits == 0) cout << "Status: New Pickup Order" << endl;
+			else cout << "Status: Wakeup-Notified" << endl;
+			cout << "Accumulated Wait Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - start).count() << "us" << endl;
+			cout << "Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Load Order: (" << pickupOrder[0] << ", " << pickupOrder[1] << ", " << pickupOrder[2] << ", " << pickupOrder[3] << ")" << endl;
 			if (buffer[0] >= pickupOrder[0]) {
 				buffer[0] -= pickupOrder[0];
 				pickupOrder[0] = 0;
@@ -214,6 +264,10 @@ void ProductWorker(int i){
 				pickupOrder[3] = 0;
 			}
 
+			cout << "Updated Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Updated Load Order: (" << pickupOrder[0] << ", " << pickupOrder[1] << ", " << pickupOrder[2] << ", " << pickupOrder[3] << ")" << endl;
+			waits++;
+
 			//Check if pickupOrder is complete
 			if (pickupOrder == fidelity) {
 				//In this case don't put it back to sleep
@@ -224,7 +278,8 @@ void ProductWorker(int i){
 				//Just print and go to next iteration
 			}
 			else {
-				//put it back to sleep
+				//put it back to sleep because not enough parts
+				//alert parts
 				cv1.notify_all();
 				goto wait;
 			}
@@ -234,20 +289,54 @@ void ProductWorker(int i){
 			//Check if entire order can be picked up. If yes then assemble
 			//If not then discard the picked out items
 
-			if (pullBufferFull(pickupOrder)) {
-				buffer[0] -= pickupOrder[0];
+			chrono::system_clock::time_point curr_time = chrono::system_clock::now();
+			cout << "Current Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - prog_start).count() << "us" << endl;
+			cout << "Product Worker ID: " << i << endl;
+			cout << "Iteration: " << endl;
+			cout << "Status: Wakeup-Timeout" << endl;
+			cout << "Accumulated Wait Time: " << chrono::duration_cast<chrono::microseconds>(curr_time - start).count() << "us" << endl;
+			//cout << "Accumulated Wait Time: " << PRODUCT_TIMEOUT << "us" << endl;
+			cout << "Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Load Order: (" << pickupOrder[0] << ", " << pickupOrder[1] << ", " << pickupOrder[2] << ", " << pickupOrder[3] << ")" << endl;
+
+			if (pullBuffer(pickupOrder)) {
+				/*buffer[0] -= pickupOrder[0];
 				pickupOrder[0] = 0;
 				buffer[1] -= pickupOrder[1];
 				pickupOrder[1] = 0;
 				buffer[2] -= pickupOrder[2];
 				pickupOrder[2] = 0;
 				buffer[3] -= pickupOrder[3];
-				pickupOrder[3] = 0;
+				pickupOrder[3] = 0;*/
+
+				if (buffer[0] >= pickupOrder[0]) {
+					buffer[0] -= pickupOrder[0];
+					pickupOrder[0] = 0;
+				}
+
+				if (buffer[1] >= pickupOrder[1]) {
+					buffer[1] -= pickupOrder[1];
+					pickupOrder[1] = 0;
+				}
+
+				if (buffer[2] >= pickupOrder[2]) {
+					buffer[2] -= pickupOrder[2];
+					pickupOrder[2] = 0;
+				}
+
+				if (buffer[3] >= pickupOrder[3]) {
+					buffer[3] -= pickupOrder[3];
+					pickupOrder[3] = 0;
+				}
 			}
 			else {
+				//Discard
 				int moveTime = pickupOrder[0] * 20 + pickupOrder[1] * 30 + pickupOrder[2] * 40 + pickupOrder[3] * 50;
 				this_thread::sleep_for(chrono::microseconds(moveTime));
 			}
+
+			cout << "Updated Buffer State: (" << buffer[0] << ", " << buffer[1] << ", " << buffer[2] << ", " << buffer[3] << ")" << endl;
+			cout << "Updated Load Order: (" << pickupOrder[0] << ", " << pickupOrder[1] << ", " << pickupOrder[2] << ", " << pickupOrder[3] << ")" << endl;
 
 			//PRINT STUFF
 
